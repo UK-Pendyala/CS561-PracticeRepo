bbe3b02ce6f3f63beefa1916f36f566261182ac7
laxmik1112 <kuntojil@oregonstate.edu>
2025-04-20 20:47:39 -0700

    added sidemenu file

---
+++ scripts/sideMenu.js
@@ -0,0 +1,11 @@
+/*************************************************************************
+ * File: sideMenu.js
+ * These functions support interaction with the side menu.
+ ************************************************************************/
+
+/*************************************************************************
+ * @function menuBtn click handler
+ * @desc 
+ * When the user clicks the menuBtn, open or close the side menu 
+ * based on current menu state.
+ *************************************************************************/
